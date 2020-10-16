70dcfdf6ba72015ab6bfc03a8572765226cd05dc
misty0123 <72991373+misty0123@users.noreply.github.com>
2020-10-17 00:55:34 +0800

    Update main.js

--- main.js
+++ main.js
@@ -9,4 +9,4 @@ client.once('ready', () => {
 });
 
 
-client.login('NzY2Njc3NDU5NTk1ODIxMTE3.X4m2Bg.-Cj32l8T7xa8Fd2UxicCqFoDK8o');
+client.login('NzY2Njc3NDU5NTk1ODIxMTE3.X4m2Bg.FQf1xPjrbdIlbqkKiZhHRBGIK0E');
