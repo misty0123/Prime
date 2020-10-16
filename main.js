917909c4c210543843a9606d553cf682c823851c
misty0123 <72991373+misty0123@users.noreply.github.com>
2020-10-17 00:48:29 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+client.once('ready', () => {
+    console.log('testbot is Online!');
+});
+
+
+client.login('NzY2Njc3NDU5NTk1ODIxMTE3.X4m2Bg.-Cj32l8T7xa8Fd2UxicCqFoDK8o');
